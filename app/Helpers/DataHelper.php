27d9abbb0ef367ea8aeaf3170be2a9cbccdb68a1
Marin Petrov <m.p.petrov@is-bg.net>
2025-12-12 15:47:32 +0200

    Update data.json path

--- app/Helpers/DataHelper.php
+++ app/Helpers/DataHelper.php
@@ -6,7 +6,12 @@ class DataHelper
 {
     public static function load()
     {
-        $path = storage_path('app/data.json');
+        $path = resource_path('data/data.json');
+
+        if (!file_exists($path)) {
+            throw new \Exception("data.json not found at: " . $path);
+        }
+
         return json_decode(file_get_contents($path), true);
     }
 }
